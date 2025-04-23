4f8b52f6e2fb261967e3ca9aab3f0df7ae99c23f
Bihan-Banerjee <bihanbanerjee26@gmail.com>
2025-04-23 16:57:39 +0530

    changed server to integrate vercel

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173","https://map-it-9g3b.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
